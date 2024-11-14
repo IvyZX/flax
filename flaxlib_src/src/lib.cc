ece5eecfe82f27109ba1eb271f7e7c811a6941e9
IvyZX <ivyzhengx@gmail.com>
2024-11-13 16:30:59 -0800

    3p build wheel change

--- flaxlib_src/src/lib.cc
+++ flaxlib_src/src/lib.cc
@@ -1,13 +1,15 @@
 #include <string>
 
-#include "third_party/nanobind/include/nanobind/nanobind.h"
+// #include "third_party/nanobind/include/nanobind/nanobind.h"
+#include "nanobind/nanobind.h"
+#include "nanobind/stl/string.h"
 
 namespace flaxlib {
 std::string sum_as_string(int a, int b) {
   return std::to_string(a + b);
 }
 
-NB_MODULE(flaxlib_cc, m) {
+NB_MODULE(flaxlib, m) {
   m.def("sum_as_string", &sum_as_string);
 }
 }  // namespace flaxlib
